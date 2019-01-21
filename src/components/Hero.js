17bc3e98c55ded123ff0ff98a9c97887b0e35089
Jonathan Spruance <jgspruance@gmail.com>
2019-01-20 18:46:31 -0800

    Add hero img, css changes, delete unused files

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <img src="energy.jpg" alt="conserve energy" />
+          <h1 className="title"></h1>
+          <h2 className="subtitle"></h2>
+        </div>
+      </div>
+    </section>
+  )
+}
